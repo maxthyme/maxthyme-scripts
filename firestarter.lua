52014743613922a2c9dedfca831e739c6fd77acb
maxthyme <simply.relativity@gmail.com>
2015-09-09 00:24:12 -0500

    Armok forgive me.
    View an item to set it on fire, view a unit to set their inventory on fire, otherwise it creates a fire at the cursor.

--- firestarter.lua
+++ firestarter.lua
@@ -6,5 +6,19 @@ local tinder
         tinder=dfhack.gui.getSelectedUnit(true).inventory
 		for k,v in ipairs(tinder) do
 			tinder[k].item.flags.on_fire=true
-		end 
+		end
+     elseif df.global.ui_advmode.menu==1 then
+	local curpos=df.global.cursor
+	df.global.world.fires:insert('#',{new=df.fire})
+	local hot = df.global.world.fires
+	 for _,k in ipairs(hot) do
+	  if k.temperature==0 then
+	   local spot = k
+		k.pos.x=curpos.x
+		k.pos.y=curpos.y
+		k.pos.z=curpos.z
+		k.timer=1000
+		k.temperature=60000
+		end
+	end
 end
