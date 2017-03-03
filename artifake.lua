d1d665fad41da2219b52bf1b657a1b6613516b32
maxthyme <simply.relativity@gmail.com>
2017-03-03 01:49:01 -0600

    Update artifake.lua
    Figured out why it kept making slabs randomly, fixed that.

--- artifake.lua
+++ artifake.lua
@@ -2,7 +2,7 @@ local utils = require 'utils'
 
 validArgs = validArgs or utils.invert({
  'help',
- 'mat',
+ 'material',
  'item',
  'name',
  'r',
@@ -17,7 +17,7 @@ if args.help then
 arguments:
     -help
         print this help message
-    -mat matstring
+    -material matstring
         specify the material of the item to be created
         examples:
             INORGANIC:IRON
@@ -50,24 +50,24 @@ if itemType == -1 then
 end
 local itemSubtype = dfhack.items.findSubtype(args.item)
 
-args.mat = dfhack.matinfo.find(args.mat)
-if not args.mat then
+args.material = dfhack.matinfo.find(args.material)
+if not args.material then
  error 'Invalid material.'
 end
 
 
-local item = dfhack.items.createItem(itemType, itemSubtype, args.mat['type'], args.mat.index, args.creator)
+local item = dfhack.items.createItem(itemType, itemSubtype, args.material['type'], args.material.index, args.creator)
 
  local base=df.item.find(df.global.item_next_id-1)
  df.global.world.artifacts.all:new()
  df.global.world.artifacts.all:insert('#',{new=df.artifact_record})
 local facts = df.global.world.artifacts.all
  for _,k in ipairs(facts) do
-  if k.id==0 then
+  if k.item==nil then
    local fake=k
    fake.id=df.global.artifact_next_id
    fake.item = {new=base}
-		fake.item.flags.artifact = true
+		fake.item.flags.artifact = false
 		fake.item.flags.artifact_mood = true
 		fake.item.id = base.id
 		fake.item.general_refs:insert('#',{new =  df.general_ref_is_artifactst})
@@ -80,20 +80,20 @@ local facts = df.global.world.artifacts.all
 		fake.item.fixed_temp = base.fixed_temp
 		fake.item.weight = base.weight
 		fake.item.weight_fraction = base.weight_fraction
-		fake.item.improvements:insert('#',{new = df.itemimprovement_spikesst,mat_type=25,mat_index=474,quality=0,skill_rating=15})
-		fake.item.improvements:insert('#',{new = df.itemimprovement_spikesst,mat_type=25,mat_index=493,quality=0,skill_rating=15})
-		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=22,mat_index=474,quality=5,skill_rating=15})
-		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=42,mat_index=480,quality=5,skill_rating=15})
-		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=22,mat_index=497,quality=5,skill_rating=15})
+--		fake.item.improvements:insert('#',{new = df.itemimprovement_spikesst,mat_type=25,mat_index=5,quality=6,skill_rating=15})
+--		fake.item.improvements:insert('#',{new = df.itemimprovement_spikesst,mat_type=25,mat_index=14,quality=6,skill_rating=15})
+		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=0,mat_index=8,quality=6,skill_rating=15})
+		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=0,mat_index=12,quality=6,skill_rating=15})
+		fake.item.improvements:insert('#',{new = df.itemimprovement_art_imagest,mat_type=4,mat_index=0,quality=6,skill_rating=15})
    fake.anon_1 = -1000000
    fake.anon_2 = -1000000
    fake.anon_3 = -1000000
-     base.flags.artifact = true
+     base.flags.artifact = false
      base.flags.artifact_mood = true
      base.general_refs = fake.item.general_refs
      base.improvements = fake.item.improvements
-     fake.item:setQuality(5)
-     base:setQuality(5)
+     fake.item:setQuality(6)
+     base:setQuality(6)
      if fake.item == 'WEAPON' then item:setSharpness(1,0) end
      if base == 'WEAPON' then item:setSharpness(1,0) end
      df.global.artifact_next_id=df.global.artifact_next_id+1
