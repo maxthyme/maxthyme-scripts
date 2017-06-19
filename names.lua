0c40db886a7da34fb170558ebd901a2517fee8ce
maxthyme <simply.relativity@gmail.com>
2017-06-19 18:18:49 -0500

    Completely overhauled interface.
    Made it detect the current target instead of needing a flag.
    Made it display a box for first name entry instead of needing a flag.
    Made it push the name to the target and make sure it is fully set upon exiting.
    
    Also drove myself nuts trying to learn about dialogs until I figured out they're just a type of editfield widget.

--- names.lua
+++ names.lua
@@ -1,55 +1,117 @@
+--rename items or units
+--[====[
+
+names
+=====
+Rename units or items.  Usage:
+
+:-help:     print this help message
+:-if a first name is desired press f, leave blank to clear current first name
+:-if viewing an artifact you can rename it
+:-if viewing a unit you can rename them
+]====]
+local gui = require 'gui'
+local dlg = require 'gui.dialogs'
+local widgets = require 'gui.widgets'
 local utils = require 'utils'
+local vw
+local trg
+local choices
 
 validArgs = validArgs or utils.invert({
  'help',
- 'item',
- 'unit',
- 'first'
 })
-
 local args = utils.processArgs({...}, validArgs)
-
 if args.help then
- print(
+	print(
 [[names.lua
 arguments:
-    -help				
-        print this help message
-    -item 					
-	if viewing an item		
-    -unit 					
-	if viewing a unit 			
-    -first Somename or "Some Names like This"
-	if a first name is desired, leave blank to clear current first name
+	-help				
+		print this help message
+	if a first name is desired press f, leave blank to clear current first name
+	if viewing an artifact you can rename it
+	if viewing a unit you can rename them
 ]])
- return
+	return
 end
-
-if not df.viewscreen_layer_choose_language_namest:is_instance(dfhack.gui.getCurViewscreen()) then
-	choices = df.new(df.viewscreen_setupadventurest) ; choices.subscreen = 3 ; gui = require 'gui' ; gui.simulateInput(choices, 'A_RANDOM_NAME') ; gui.simulateInput(choices, 'A_CUST_NAME')
-end
-
-if args.item then
-	fact = dfhack.gui.getCurViewscreen().parent.item.general_refs[0].artifact_id
-	trg = df.artifact_record.find(fact)
-end
- 
-if args.unit then
-	trg = dfhack.gui.getCurViewscreen().parent.unit
-end
-
-if args.first then
-	trg.name.first_name = args.first
-end
-
-function newName()
- 	newn = dfhack.gui.getCurViewscreen().name
-	oldn = trg.name
-	for k = 0,6 do
-	 	oldn.words[k] = newn.words[k]
- 		oldn.parts_of_speech[k] = newn.parts_of_speech[k]
+namescr = defclass(namescr, gui.Screen)
+namescr.focus_path = 'names'
+function namescr:init()
+	if not dfhack.gui.getViewscreenByType(df.viewscreen_layer_choose_language_namest)==true then
+		self:addviews{
+			widgets.Label{
+				view_id='namescr',
+				frame = {b=4, l=1},
+				text = {
+					{text = "Press f to Change First Name"},NEWLINE,
+					{text = "Press Esc to Set Name and Exit"},
+						},
+					}
+				}
+		vw = dfhack.gui.getCurViewscreen()
+		if df.viewscreen_itemst:is_instance(vw.parent) then
+			fact = vw.parent.item.general_refs[0].artifact_id
+			trg = df.artifact_record.find(fact)
+		elseif df.viewscreen_dungeon_monsterstatusst:is_instance(vw.parent) then
+			uid = vw.parent.unit.id
+			trg = df.unit.find(uid)
+		elseif df.global.ui_advmode.menu==1 then
+			local t_look=df.global.ui_look_list.items[df.global.ui_look_cursor]
+			if t_look.type==2 then
+				trg=t_look.unit
+			end
+		end
+		choices = df.viewscreen_setupadventurest:new()
+		choices.page = 7
+		local tn = choices.adventurer
+		utils.assign(tn.name, trg.name)
+		gui.simulateInput(choices, 'A_CUST_NAME')
+		vw = dfhack.gui.getViewscreenByType(df.viewscreen_layer_choose_language_namest)
+	elseif dfhack.gui.getViewscreenByType(df.viewscreen_layer_choose_language_namest)==true then
+		qerror('names screen already displayed')
 	end
- 	oldn.language = newn.language
- 	oldn.has_name = newn.has_name
+	return vw,trg
 end
-newName()
+function namescr:setName()
+	for k = 0,6 do
+		trg.name.words[k] = vw.name.words[k]
+		trg.name.parts_of_speech[k] = vw.name.parts_of_speech[k]
+		trg.name.language = vw.name.language
+		trg .name.has_name = vw.name.has_name
+	end
+end
+function namescr:setFirst()
+local str = ''
+	dlg.showInputPrompt("Set First Name?","First: ",COLOR_WHITE,'',
+		function(str)
+			if str==nil then
+				self:callback("setFirst")
+			else
+				vw.name.first_name = str
+				trg.name.first_name = str
+			end
+		end)
+	return str
+end
+function namescr:onRenderBody(dc)
+    self._native.parent:render()
+end
+function namescr:onInput(keys)
+	if keys.SELECT then
+		self:setName()
+	end
+	if keys.CUSTOM_F then
+		self:setFirst()
+		if not str==nil then
+			trg.name.first_name = str
+		end
+	end
+	if keys.LEAVESCREEN then
+		self:setName()
+		self:dismiss()
+		dfhack.screen.dismiss(self._native.parent)
+	end
+	return
+	gui.simulateInput(self._native.parent, keys)
+end
+namescr():show()
